ff235da5a7ee5596c57c6f8f9fa751238a37f4d9
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-08-18 12:15:59 +0100

    back to one file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,33 @@ class hunspell (
 	$ensure = 'present',
 ){
 
-	class { 'hunspell::install': }
+	if $hunspell::ensure != 'absent' {
+
+		package { "hunspell":
+			ensure => installed
+		}
+
+		package { "hunspell-en-us":
+			ensure  => installed,
+			require => Package["hunspell"],
+		}
+
+		package { "myspell-en-gb":
+			ensure  => installed,
+			require => Package["hunspell"],
+		}
+
+		# NZ Dictionaries
+		file { "/usr/share/hunspell/":
+			ensure  => directory,
+			owner   => 'root',
+			group   => 'root',
+			mode    => '0644',
+			recurse => true,
+			source  => "puppet:///modules/hunspell/dictionaries/",
+			require => Package["hunspell"],
+		}
+
+	}
 
 }
