c6e6542ae942e599cc3437006c4e6321d2b129f4
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-08-18 12:45:48 +0100

    remove if

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,33 +2,22 @@ class hunspell (
 	$ensure = 'present',
 ){
 
-	if $hunspell::ensure != 'absent' {
-
-		package { "hunspell":
-			ensure => installed
-		}
-
-		package { "hunspell-en-us":
-			ensure  => installed,
-			require => Package["hunspell"],
-		}
-
-		package { "myspell-en-gb":
-			ensure  => installed,
-			require => Package["hunspell"],
-		}
-
-		# NZ Dictionaries
-		file { "/usr/share/hunspell/":
-			ensure  => directory,
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			recurse => true,
-			source  => "puppet:///modules/hunspell/dictionaries/",
-			require => Package["hunspell"],
-		}
-
+	package {
+		"hunspell":	ensure => installed;
+		"hunspell-en-us":	ensure  => installed;
+		"myspell-en-gb":	ensure  => installed;
 	}
+
+	# NZ Dictionaries
+	file { "/usr/share/hunspell/":
+		ensure  => directory,
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0644',
+		recurse => true,
+		source  => "puppet:///modules/hunspell/dictionaries/",
+		require => Package["hunspell"],
+	}
+
 
 }
