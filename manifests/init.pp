7a40932844d4d055d6d3188a82c7287018f1731b
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-08-18 09:59:58 +0100

    add dictionary files and init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+class hunspell {
+
+	package { "hunspell":
+		ensure => installed
+	}
+
+	package { "hunspell-en-us":
+		ensure => installed,
+		require => Package["hunspell"],
+	}
+
+	package { "myspell-en-gb":
+		ensure => installed,
+		require => Package["hunspell"],
+	}
+
+	# NZ Dictionaries
+	file { "/usr/share/hunspell/":
+		ensure => directory,
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+		recurse => true,
+		source => "puppet:///modules/hunspell/dictionaries/",
+		require => Package["hunspell"],
+	}
+}
